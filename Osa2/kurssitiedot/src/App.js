88481967cf35956579b0263cc9c12d17dd664144
Timo Laitila <timo.t.laitila@gmail.com>
2021-04-24 23:27:52 +0300

    Tehtävä 2.1

---
+++ Osa2/kurssitiedot/src/App.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import Course from './components/Course'
+
+const Total = (props) => {
+  return (
+    <p>Number of exercises {props.course.parts[0].exercises + props.course.parts[1].exercises + props.course.parts[2].exercises}</p>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    id: 1,
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      },
+      {
+        name: 'State of art',
+        exercises: 4,
+        id: 4
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Course course={course} />
+    </div>
+  )
+}
+
+export default App
---
+++ Osa2/kurssitiedot/src/components/Course.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Header = ({name}) => <h1>{name}</h1>
+
+const Part = ({name, exercises}) => <p>{name} {exercises}</p>
+
+const Content = ({parts}) => {
+  return (
+    <div>
+    {parts.map(
+      part =>
+        <Part key={part.id} name={part.name} exercises={part.exercises} />
+    )}
+    </div>
+  )
+}
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+    </div>
+  )
+}
+
+export default Course
