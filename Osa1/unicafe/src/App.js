2b16e0516cd8d66a081f46f44ef0abe4723fb37d
Timo Laitila <timo.t.laitila@gmail.com>
2021-04-24 12:18:30 +0300

    Tehtävä 1.11*

--- Osa1/unicafe/src/App.js
+++ Osa1/unicafe/src/App.js
@@ -11,7 +11,6 @@ const Statistics = ({good, bad, neutral}) => {
   if (all > 0) {
     return (
     <div>
-      <h1>statistics</h1>
       <StatisticLine text="good" value ={good} />
       <StatisticLine text="neutral" value ={neutral} />
       <StatisticLine text="bad" value ={bad} />
@@ -24,7 +23,36 @@ const Statistics = ({good, bad, neutral}) => {
 
   return (
       <div>
-        <h1>statistics</h1>
+        <div>No feedback given</div>
+      </div>
+  )
+}
+
+const StatisticRow = ({text, value}) => <tr><td>{text}</td><td>{value}</td></tr>
+
+const StatisticsTable = ({good, bad, neutral}) => {
+  let all
+
+  all = good + bad + neutral
+
+  if (all > 0) {
+    return (
+    <div>
+      <table>
+        <tbody>
+          <StatisticRow text="good" value ={good} />
+          <StatisticRow text="neutral" value ={neutral} />
+          <StatisticRow text="bad" value ={bad} />
+          <StatisticRow text="all" value ={all} />
+          <StatisticRow text="average" value ={(-1*bad+good)/all} />
+          <StatisticRow text="positive" value ={100 * good / all + ' %'} />
+        </tbody>
+      </table>
+    </div>
+    )
+  }
+  return (
+      <div>
         <div>No feedback given</div>
       </div>
   )
@@ -45,7 +73,8 @@ const App = () => {
       <Button handleClick={() => setGood(good+1)} text="good"/>
       <Button handleClick={() => setNeutral(neutral+1)} text="neutral"/>
       <Button handleClick={() => setBad(bad+1)} text="good"/>
-      <Statistics good={good} bad={bad} neutral={neutral}/>
+      <h1>statistics</h1>
+      <StatisticsTable good={good} bad={bad} neutral={neutral}/>
     </div>
 
   )
