f63397bf8a667ce4bc19b32a372e3b4439cdb388
Timo Laitila <timo.t.laitila@gmail.com>
2021-04-25 21:57:19 +0300

    Tehtävä 2.11

--- Osa2/puhelinluettelo/src/App.js
+++ Osa2/puhelinluettelo/src/App.js
@@ -1,21 +1,42 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import Persons from './components/Persons'
 import PersonForm from './components/PersonForm'
 import Filter from './components/Filter'
 
 const App = () => {
 
+
+  const endpoint = 'http://localhost:3001/persons'
+
   // Test persons
+  /*
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas', number: '040-123456' },
     { name: 'Ada Lovelace', number: '39-44-5323523' },
     { name: 'Dan Abramov', number: '12-43-234345' },
     { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ])
+  */
+
+  const [persons, setPersons] = useState([])
 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
-  const [ visible, setVisible ] = useState(persons)
+  const [ visible, setVisible ] = useState([])
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get(endpoint)
+      .then(response => {
+        console.log('promise fulfilled')
+        console.log(response.data)
+        setPersons(response.data)
+        setVisible(response.data)
+      })
+  }, [])
+
 
   return (
     <div>
