bcc004f587d15b6b98e17c510dfcc90f68da2db5
Timo Laitila <timo.t.laitila@gmail.com>
2021-05-10 20:46:18 +0300

    Tehtävä 4.4

--- Osa4/bloglistbackend/utils/list_helper.js
+++ Osa4/bloglistbackend/utils/list_helper.js
@@ -1,8 +1,16 @@
-const dummy = (blogs) => {
+const dummy = () => {
   // ...
   return 1
 }
 
+
+const totalLikes = (blogs) => {
+  var sum = blogs.reduce(function(total, blog) {
+    return total+blog.likes
+  },0)
+  return sum
+}
+
 module.exports = {
-  dummy
+  dummy, totalLikes
 }
